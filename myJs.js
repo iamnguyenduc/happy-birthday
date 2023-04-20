c316e4bd9e2ab53501436cc1ba48c6bbe469ee3f
iamnguyenduc <85486907+iamnguyenduc@users.noreply.github.com>
2023-04-20 19:43:11 +0700

    Update myJs.js

--- myJs.js
+++ myJs.js
@@ -8,7 +8,7 @@ const textConfig = {
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
   text9: "Vì cậu đẹp try và dthuong vlll:>>",
-  text10: "Tớ biết mà ^^ Yêu cậux 1000.000,
+  text10: "Tớ biết mà ^^ Yêu cậux 1000.000",
   text11:
     "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
